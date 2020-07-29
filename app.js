59b4212e4fb2a8f41917ce53bf3636df75fdf86c
Quoc <tqviet.0503@gmail.com>
2020-07-29 14:47:07 +0700

    add test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
-const corsMiddleware = require('./middlewares/cors')
+//const corsMiddleware = require('./middlewares/cors')
+var cors = require('cors')
 const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./config/keys')
 
@@ -13,7 +14,8 @@ mongoose.connect(MONGOURI,
          .catch(err => console.log("MongoDB can't connect", err)
     )
 
-app.use(corsMiddleware); 
+app.use(cors())
+//app.use(corsMiddleware); 
 
 //set
 app.use(express.json());
