62717d13b2f4c0cf9181f8f2aae57aa00ca2dc42
Quoc <tqviet.0503@gmail.com>
2020-07-29 13:46:06 +0700

    add test

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,14 +6,25 @@ let whitelist = [
 if(process.env.NODE_ENV === 'development') {
     whitelist.push('http://localhost:5000')
 }
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+
+var corsOptions = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
     }
+    callback(null, corsOptions) // callback expects two parameters: error and options
   }
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+//   }
 const middleware = cors(corsOptions)
 module.exports = middleware
