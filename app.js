af257b61d06318d5108b7bdac69cddcdb942b7cd
Quoc <tqviet.0503@gmail.com>
2020-08-06 00:56:06 +0700

    Change cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const app = express();
+var cors = require('cors')
+
 const mongoose = require('mongoose');
 //const corsMiddleware = require('./middlewares/cors')
-var cors = require('cors')
+const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./config/keys')
 
@@ -14,7 +16,7 @@ mongoose.connect(MONGOURI,
          .catch(err => console.log("MongoDB can't connect", err)
     )
 
-app.use(cors())
+
 //app.use(corsMiddleware); 
 
 //set
